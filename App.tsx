c0a412b205a549f5cf4a23db1d14cda145603242
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-09-26 09:08:19 +0200

    add button with click handler and update styles in App component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const handleClick = (value: number) => {
+    console.warn(value);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.text}>Todos</Text>
+        <Button title="Click Me" onPress={() => handleClick(7)}/>
+      </View>
+    </>
+    // {<View style={styles.container}>}
+    //   {/*<Text>Todos</Text>*/}
+    //   {/*<StatusBar style="auto"/>*/}
+    // {/*</View>*/}
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  text: {
+    color: "purple",
+  }
 });
+
+export default App;
